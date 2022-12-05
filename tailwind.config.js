a413b2ccc56f4639766c2d2250ce01f3b539d709
Megan <chengmeg@msu.edu>
2022-12-04 22:19:54 -0500

    added everything but the js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         brightRedSupLight: 'hsl(12,88%,95%)',
         darkBlue: 'hsl(228,39%,23%)',
         darkGrayistBlue: 'hsl(227,12%,61%)',
-        veryDarlBlue: 'hsl(223,12%,13%)',
+        veryDarkBlue: 'hsl(223,12%,13%)',
         veryPaleRed: 'hsl(13,100%,96%)',
         veryLightGray: 'hsl(0,0%,98%)',
       }
